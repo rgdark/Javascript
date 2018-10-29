3a171ec2a57a5b8090d598f6ce693feaf98ad5da
rgdark <rgdark2363@gmail.com>
2018-10-29 12:28:13 -0700

    getOddLengthWordsAtProperty.js
    Given an object and a key, "getOddLengthWordsAtProperty" returns an array containing all the odd length word elements of the array located at the given key.

---
+++ getOddLengthWordsAtProperty.js
@@ -0,0 +1,31 @@
+/*
+Write a function called "getOddLengthWordsAtProperty".
+
+Given an object and a key, "getOddLengthWordsAtProperty" returns an array containing all the odd length word elements of the array located at the given key. 
+
+Notes:
+* If the array is empty, it should return an empty array.
+* If it contains no odd length elements, it should return an empty array.
+* If the property at the given key is not an array, it should return an empty array.
+* If there is no property at the given key, it should return an empty array.
+
+var obj = {
+  key: ['It', 'has', 'some', 'words']
+};
+var output = getOddLengthWordsAtProperty(obj, 'key');
+console.log(output); // --> ['has', 'words']
+*/
+
+function getOddLengthWordsAtProperty(obj, key) {
+  // your code here
+if(!obj[key] || !Array.isArray(obj[key]) || obj[key].length==0)
+  {
+    return [];
+  }
+  output=obj[key].filter(function(element){
+    return element.length%2!=0; 
+  });
+  return output;
+  
+}
+
