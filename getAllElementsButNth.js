56e4fd72f03b8b9f46b48956d92f03f29be13fe4
rgdark <rgdark2363@gmail.com>
2018-10-29 12:42:45 -0700

    getAllElementsButNth.js

---
+++ getAllElementsButNth.js
@@ -0,0 +1,16 @@
+/*
+Write a function called "getAllElementsButNth".
+
+Given an array and an index, "getAllElementsButNth" returns an array with all the elements but the nth.
+
+var output = getAllElementsButNth(['a', 'b', 'c'], 1);
+console.log(output); // --> ['a', 'c']
+*/
+
+function getAllElementsButNth(array, n) {
+  // your code here
+  array.splice(n,1);
+  return array;
+
+}
+
